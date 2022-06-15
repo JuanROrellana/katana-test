cbe7f5a02cccebd9ce192511465e494d4b96b396
Juan Jose Ramirez Orellana <ramirez.orellana.juanjose@gmail.com>
2022-06-15 00:16:30 -0600

    Add swagger support

--- server/src/decks/dtos/create-deck.dto.ts
+++ server/src/decks/dtos/create-deck.dto.ts
@@ -1,11 +1,13 @@
 import { IsBoolean, IsEnum } from 'class-validator';
-import { DeckTypeEnum } from '../deck.entity';
+
 import { ApiProperty } from '@nestjs/swagger';
+import { DeckTypeEnum } from '../deck.entity';
 
 export class CreateDeckDto {
   @ApiProperty({
     example: 'SHORT',
-    description: 'The deck created can be SHORT OR FULL. Short will be 32 cards (lacking cards from 2 to 6).',
+    description:
+      'The deck created can be SHORT OR FULL. Short will be 32 cards (lacking cards from 2 to 6).',
   })
   @IsEnum(DeckTypeEnum, { each: true })
   type: DeckTypeEnum;
