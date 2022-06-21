6cfb2ffdec62a5bca3c77e31807d0762b4662068
Juan Jose Ramirez Orellana <ramirez.orellana.juanjose@gmail.com>
2022-06-20 20:40:14 -0600

    Improve Insert Service

---
+++ server/src/common/contants/shortDeckValues.constant.ts
@@ -0,0 +1,10 @@
+export const shortDeckValues = [
+  'ACE',
+  '7',
+  '8',
+  '9',
+  '10',
+  'JACK',
+  'QUEEN',
+  'KING',
+];
--- server/src/decks/decks.service.ts
+++ server/src/decks/decks.service.ts
@@ -4,6 +4,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Deck, DeckTypeEnum } from './deck.entity';
 import { CreateDeckDto } from './dtos/create-deck.dto';
 import { Card, SuitEnum } from './card.entity';
+import { fullDeckValues } from '../common/contants/fullDeckValues.constant';
+import { shortDeckValues } from '../common/contants/shortDeckValues.constant';
 
 @Injectable()
 export class DecksService {
@@ -12,43 +14,16 @@ export class DecksService {
     @InjectRepository(Card) private cardRepo: Repository<Card>,
   ) {}
 
-  deckValues: string[] = [
-    'ACE',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    'JACK',
-    'QUEEN',
-    'KING',
-  ];
-
-  shortDeckValues: string[] = [
-    'ACE',
-    '7',
-    '8',
-    '9',
-    '10',
-    'JACK',
-    'QUEEN',
-    'KING',
-  ];
-
   async createDeck(createDeckDto: CreateDeckDto) {
     if (createDeckDto.shuffled) {
-      if (createDeckDto.type == DeckTypeEnum.FULL){
-        this.deckValues.sort(function (a, b) {
+      if (createDeckDto.type == DeckTypeEnum.FULL) {
+        fullDeckValues.sort(function () {
           return 0.5 - Math.random();
         });
       }
 
-      if (createDeckDto.type == DeckTypeEnum.SHORT){
-        this.shortDeckValues.sort(function (a, b) {
+      if (createDeckDto.type == DeckTypeEnum.SHORT) {
+        shortDeckValues.sort(function () {
           return 0.5 - Math.random();
         });
       }
@@ -64,10 +39,21 @@ export class DecksService {
     return deck;
   }
 
-  getCards(deckType: DeckTypeEnum, deck: Deck): Promise<Card[]> {
-    return deckType == DeckTypeEnum.FULL
-      ? this.getFullCards(deck)
-      : this.getShortCards(deck);
+  async getCards(deckType: DeckTypeEnum, deck: Deck): Promise<Card[]> {
+    let deckToLoop: string[];
+    const cards: Card[] = [];
+    if (deckType == DeckTypeEnum.SHORT) {
+      deckToLoop = shortDeckValues;
+    } else {
+      deckToLoop = fullDeckValues;
+    }
+    for (let idx = 0; idx < deckToLoop.length; idx++) {
+      cards.push(await this.saveCard(deckToLoop[idx], SuitEnum.DIAMONDS, deck));
+      cards.push(await this.saveCard(deckToLoop[idx], SuitEnum.HEARTS, deck));
+      cards.push(await this.saveCard(deckToLoop[idx], SuitEnum.CLUBS, deck));
+      cards.push(await this.saveCard(deckToLoop[idx], SuitEnum.SPADES, deck));
+    }
+    return cards;
   }
 
   getRemaining(deckType: DeckTypeEnum): number {
@@ -77,41 +63,5 @@ export class DecksService {
   saveCard(value: string, suit: SuitEnum, deck: Deck): Promise<Card> {
     const card = this.cardRepo.create({ value, suit, deck });
     return this.cardRepo.save(card);
-  }
-
-  async getFullCards(deck: Deck, cards: Card[] = []): Promise<Card[]> {
-    for (let idx = 0; idx < this.deckValues.length; idx++) {
-      cards.push(
-        await this.saveCard(this.deckValues[idx], SuitEnum.DIAMONDS, deck),
-      );
-      cards.push(
-        await this.saveCard(this.deckValues[idx], SuitEnum.HEARTS, deck),
-      );
-      cards.push(
-        await this.saveCard(this.deckValues[idx], SuitEnum.CLUBS, deck),
-      );
-      cards.push(
-        await this.saveCard(this.deckValues[idx], SuitEnum.SPADES, deck),
-      );
-    }
-    return cards;
-  }
-
-  async getShortCards(deck: Deck, cards: Card[] = []): Promise<Card[]> {
-    for (let idx = 0; idx < this.shortDeckValues.length; idx++) {
-      cards.push(
-        await this.saveCard(this.shortDeckValues[idx], SuitEnum.DIAMONDS, deck),
-      );
-      cards.push(
-        await this.saveCard(this.shortDeckValues[idx], SuitEnum.HEARTS, deck),
-      );
-      cards.push(
-        await this.saveCard(this.shortDeckValues[idx], SuitEnum.CLUBS, deck),
-      );
-      cards.push(
-        await this.saveCard(this.shortDeckValues[idx], SuitEnum.SPADES, deck),
-      );
-    }
-    return cards;
   }
 }
